740a60e323a52dfe564a81599a2aa9b4990c203e
Yle0537 <le0537@outlook.com>
2024-12-22 17:11:09 +0800

    完善部分信息

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,31 +1,35 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 const base = "/le-docs/"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base,
-  title: "樂码",
-  description: "樂码的官方文档库",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+    base,
+    title: "樂码",
+    description: "樂码的官方文档库",
+    head: [
+        ['link', {rel: 'icon', href: './favicon.ico'}]
     ],
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: './favicon.ico',
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '示例', link: '/markdown-examples'}
+        ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+        sidebar: [
+            {
+                text: 'Examples',
+                items: [
+                    {text: 'Markdown Examples', link: '/markdown-examples'},
+                    {text: 'Runtime API Examples', link: '/api-examples'}
+                ]
+            }
+        ],
+
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/Yle0537/le-docs'}
         ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    }
 })
