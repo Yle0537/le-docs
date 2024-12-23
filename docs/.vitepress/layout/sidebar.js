dff6f6c09e97bc9f733fc0e06ae9117a16dc23f0
Yle0537 <le0537@outlook.com>
2024-12-23 23:43:42 +0800

    修补

--- docs/.vitepress/layout/sidebar.js
+++ docs/.vitepress/layout/sidebar.js
@@ -16,7 +16,6 @@ export const sidebar = {
                 {text: "算法简介", link: "/md/算法竞赛/算法/算法简介"},
                 {text: "常用模板", link: "/md/算法竞赛/算法/常用模板"},
                 {text: "基础", link: "/md/算法竞赛/算法/基础"},
-                {text: "常用模板", link: "/md/算法竞赛/算法/常用模板"},
                 {text: "双指针", link: "/md/算法竞赛/算法/双指针"},
             ]
         }
